de212377120465e9866204a105a8d5caaf324e26
Prabhat-2101 <prabhatkumarraj2003@gmail.com>
2025-05-12 22:52:56 +0530

    blog-model-schema-updated

--- app.js
+++ app.js
@@ -4,20 +4,35 @@ const connectDB = require("./config/db.js");
 const authRoutes = require("./routes/authRoutes.js");
 const blogRoutes = require("./routes/blogRoutes.js");
 const cors = require("cors");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 
 dotenv.config();
 connectDB();
 
+const _dirname = path.dirname('');
+const buildPath = path.join(_dirname, '../blgo-frontend/build');
+
 const app = express();
 app.use(express.json());
+app.use(express.static(buildPath));
+const isProd = process.env.NODE_ENV === 'production';
+const allowedOrigins = isProd
+  ? ['https://localhost:5173']
+  : ['http://localhost:5173', 'http://192.168.0.108:5173'];
 
 app.use('/*',cors({
-    origin: 'http://localhost:5173', // frontend URL
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+        } else {
+        callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true // required to send cookies
 }));
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
-app.route("api/v1/blog",blogRoutes);
+app.use("/api/v1/blog",blogRoutes);
 
 module.exports =  app;
--- models/Blog.js
+++ models/Blog.js
@@ -2,11 +2,26 @@ const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, required: true },  // You can keep this for a short summary
     content: { type: String, required: true },
-    image: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    image: {
+        type: String,
+        default: 'https://vignan.ac.in/vignantest/departments/dep_assets/images/no-image.png'
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
     approved: { type: Boolean, default: false },
+    tags: {
+        type: [String],
+        default: []
+    },
+    upvote: {
+        type: Number,
+        default: 0
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Blog", blogSchema);
